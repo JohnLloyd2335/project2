6866034f6384088bca84c66c1cdf58740f69bc41
John Lloyd Lacadin <johnlloydlacadin42301@gmail.com>
2022-07-29 15:48:18 +0800

    laravel 9 crud

---
+++ resources/views/posts/create.blade.php
@@ -0,0 +1,48 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('New Post') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 bg-white border-b border-gray-200">
+
+                    @if ($errors->any())
+                        <div class="alert alert-danger">
+                            <ul>
+                                @foreach ($errors->all() as $error)
+                                    <li>{{ $error }}</li>
+                                @endforeach
+                            </ul>
+                        </div>
+                    @endif
+                    
+                    <form action="{{ route('posts.store') }}" method="post">
+                        @csrf
+                        Title:
+                        <br>
+                        <input  class="block font-medium text-sm text-gray-700" type="text" name="title"  >
+                        <br>
+                        Post Text:
+                        <input  class="block font-medium text-sm text-gray-700" type="text" name="post_text">
+                        <br>
+                        Category
+                        <select  name="category" id="">
+                            @foreach ($categories as $category)
+                                <option value="{{ $category->id }}">{{ $category->name }}</option>
+                            @endforeach
+                            
+                        </select>
+                        <br><br>
+                        <button class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring ring-gray-300 disabled:opacity-25 transition ease-in-out duration-150" type="submit">Save </button>
+                       
+
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
---
+++ resources/views/posts/edit.blade.php
@@ -0,0 +1,34 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Category Edit') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 bg-white border-b border-gray-200">
+                    <form action="{{ route('posts.update', $post) }}" method="post">
+                        @method('PUT')
+                        @csrf
+                        Name:
+                        <br>
+                        <input required class="block font-medium text-sm text-gray-700" type="text" name="title" value=" {{ $post->title }} ">
+                        <input required class="block font-medium text-sm text-gray-700" type="text" name="post_text" value=" {{ $post->post_text }} ">
+                        <select required name="category">
+                            @foreach ($categories as $category)
+                                <option value="{{ $category->id }}"
+                                    @selected($category->id == $post->category_id)>{{ $category->name }}</option>
+                            @endforeach
+                        </select>
+                        <br><br>
+                        <button class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring ring-gray-300 disabled:opacity-25 transition ease-in-out duration-150" type="submit">Save </button>
+                       
+
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
